472f47ba9e902e16445316a42a35584a2af46bbe
Harshkr007 <harshkr1890@gmail.com>
2024-07-01 22:15:17 +0530

    card cotext Switcher

--- 08miniContext/src/Context/UserContext.js
+++ 08miniContext/src/Context/UserContext.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import { createContext } from "react"; not needed here as we used React.creteContext()
 
 const UserContext = React.createContext();
 
---
+++ 09themeSwitcher/src/context/theme.js
@@ -0,0 +1,16 @@
+import { createContext,useContext } from "react"
+
+const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {}
+});
+
+const ThemeProvider = ThemeContext.Provider;
+
+function useTheme() {
+    return useContext(ThemeContext);
+}
+
+export default useTheme;
+export {ThemeProvider,ThemeContext};
